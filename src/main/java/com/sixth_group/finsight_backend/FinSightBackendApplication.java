259e3d550b28ff467714e516a764a6f8d9a950df
lpf-330 <3360970133@qq.com>
2026-04-22 11:52:40 +0800

    初始化项目

---
+++ src/main/java/com/sixth_group/finsight_backend/FinSightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sixth_group.finsight_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSightBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinSightBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sixth_group/finsight_backend/FinSightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixth_group.finsight_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSightBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
